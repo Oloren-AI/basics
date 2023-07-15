642a7fcc44f64653bc1f6167d324b9612eb0ff00
David Huang <david.zq.huang@gmail.com>
2023-07-15 18:40:41 -0400

    Downlaod file as in

---
+++ src/nodes/downloadasin/Applet.tsx
@@ -0,0 +1,38 @@
+/**
+ * This demonstrates the use of "done" functionality and also shows how we can use "hidden" applets
+ * to execute some bare minimum backend-like code on the frontend.
+ */
+
+import { DownloadOutlined } from "@ant-design/icons";
+import { ApplicationProps, Json } from "@oloren/core";
+import { Button } from "antd";
+import React, { useEffect } from "react";
+
+import { NodeData } from "./Base"
+
+function Applet({
+  node,
+  inputs,
+  setOutputs,
+  download,
+}: ApplicationProps<NodeData, [{ url: string }, string], []>) {
+  useEffect(() => {
+    setOutputs([]);
+  }, []);
+
+  return (
+    <div tw="w-full">
+      <Button
+        tw="w-full"
+        icon={<DownloadOutlined />}
+        onClick={() => {
+          download(inputs[0].url, inputs[1]);
+        }}
+      >
+        {`Download File (${inputs[1]})`}
+      </Button>
+    </div>
+  );
+}
+
+export default Applet;
---
+++ src/nodes/downloadasin/Base.tsx
@@ -0,0 +1,18 @@
+import { NodeProps } from "@oloren/core";
+import React from "react";
+import { Input, Typography } from "antd";
+
+export type NodeData = {
+  initial: string;
+}
+
+function Base({node, setNode}: NodeProps<NodeData>) {
+  return (
+    <div tw="w-[400px]">
+      <Typography.Text>Download File As (file is first input, file name is the second input)</Typography.Text>
+    </div>
+  );
+}
+
+export default Base;
+
